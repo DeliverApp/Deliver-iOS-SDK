17b1e9c98d8bcddd5848ac0f62c7e4378ecbc195
Benjamin FROLICHER <benjamin.frolicher@gmail.com>
2025-06-03 15:09:33 +0200

    feat: move to xcframework

--- Package.swift
+++ Package.swift
@@ -8,23 +8,12 @@ let package = Package(
         .iOS(.v14)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DeliverSDK",
-            type: .dynamic,
             targets: ["DeliverSDK"]
         )
     ],
-
     targets: [
-        .target(
-            name: "DeliverSDK",
-            path: "DeliverSDK",
-            resources: [
-                .process("core/resources/fonts"),
-                .process("core/Deliver.xcassets"),
-            ]
-        )
+        .binaryTarget(name: "DeliverSDK", path: "./DeliverSDK.xcframework")
     ]
 )
-
